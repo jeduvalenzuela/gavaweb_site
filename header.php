d5091dbcac3c9a3e5cc2d2ba2e10a11b0618c251
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-03-19 18:07:06 -0300

    import files

---
+++ header.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from static.laralink.com/html/arino/digital-agency.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Feb 2024 14:57:02 GMT -->
+<head>
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Laralink">
+  <!-- Favicon Icon -->
+  <link rel="icon" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/favicon.png">
+  <!-- Site Title -->
+  <title>GAVAWEB.com - Web Design + Digital Marketing</title>
+  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/plugins/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/plugins/fontawesome.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/plugins/slick.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/plugins/lightgallery.min.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/plugins/animate.css">
+  <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-+6I+qvGs2dG09LjMzON6qmxIig+ZZ3bJhw2yLuMzWcDpHqQwPIoKR9s1/9r3eE6U7MFQC9UDTPl3MzgkgAhcYQ==" crossorigin="anonymous" />
+
+</head>
+
+<body>
+  <div class="cs-preloader cs-center">
+    <div class="cs-preloader_in"></div>
+  </div>
+  <!-- Start Header Section -->
+  <header class="cs-site_header cs-style1 text-uppercase cs-sticky_header">
+    <div class="cs-main_header">
+      <div class="container">
+        <div class="cs-main_header_in">
+          <div class="cs-main_header_left">
+            <a class="cs-site_branding" href="index-2.html">
+              <img src="http://gavaweb.com/wp-content/uploads/2023/05/logo-black.png" alt="Logo">
+            </a>
+          </div>
+          <div class="cs-main_header_center">
+            <div class="cs-nav cs-primary_font cs-medium">
+              <ul class="cs-nav_list">
+                <!--li class="menu-item-has-children cs-mega_menu">
+                  <a href="<?php home_url( '/' ); ?>">Home</a>
+                  <ul class="cs-mega_wrapper">
+                    <li>
+                      <ul>
+                        <li>
+                          <a href="index-2.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/main-home.jpg" alt="Thumb">
+                          </a>
+                          <a href="index-2.html">Main Home</a>
+                        </li>
+                        <li>
+                          <a href="architecture-agency.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/architecture-agency.jpg" alt="Thumb">
+                          </a>
+                          <a href="architecture-agency.html">Architecture Agency</a>
+                          <span class="cs-header_badge">New</span>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <ul>
+                        <li>
+                          <a href="photography-agency.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/photography-agency.jpg" alt="Thumb">
+                          </a>
+                          <a href="photography-agency.html">Photography Agency</a>
+                        </li>
+                        <li>
+                          <a href="creative-solution.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/creative-solution.jpg" alt="Thumb">
+                          </a>
+                          <a href="creative-solution.html">Creative Solution</a>
+                          <span class="cs-header_badge">New</span>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <ul>
+                        <li>
+                          <a href="creative-portfolio.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/portfolio.jpg" alt="Thumb">
+                          </a>
+                          <a href="creative-portfolio.html">Creative Portfolio</a>
+                        </li>
+                        <li>
+                          <a href="personal-portfolio.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/personal-portfolio.jpg" alt="Thumb">
+                            <span class="cs-header_badge">New</span>
+                          </a>
+                          <a href="personal-portfolio.html">Personal Portfolio</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <ul>
+                        <li>
+                          <a href="digital-agency.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/digital-agency.jpg" alt="Thumb">
+                          </a>
+                          <a href="digital-agency.html">Digital Agency</a>
+                        </li>
+                        <li>
+                          <a href="https://arino-html-rtl.vercel.app/" target="_blank" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/rtl.jpg" alt="Thumb">
+                          </a>
+                          <a href="https://arino-html-rtl.vercel.app/" target="_blank">RTL Demo</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <ul>
+                        <li>
+                          <a href="marketing-agency.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/marketing.jpg" alt="Thumb">
+                          </a>
+                          <a href="marketing-agency.html">Marketing Agency</a>
+                        </li>
+                        <li>
+                          <a href="showcase-portfolio.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/portfolio-showcase.jpg" alt="Thumb">
+                          </a>
+                          <a href="showcase-portfolio.html">Portfolio Showcase</a>
+                        </li>
+                        <li>
+                          <a href="case-study-showcase.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/case-study-showcase.jpg" alt="Thumb">
+                          </a>
+                          <a href="case-study-showcase.html">Case Study Showcase</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <ul>
+                        <li>
+                          <a href="freelancer-agency.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/freelancer-agency.jpg" alt="Thumb">
+                          </a>
+                          <a href="freelancer-agency.html">Freelancing Agency</a>
+                        </li>
+                        <li>
+                          <a href="video-showcase.html" class="cs-nav_list_img">
+                            <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/demo/video-showcase.jpg" alt="Thumb">
+                            <span class="cs-header_badge">New</span>
+                          </a>
+                          <a href="video-showcase.html">Video Showcase</a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li-->
+                <li><a href="about.html">Home</a></li>
+                <!--li><a href="about.html">About</a></li>
+                <li class="menu-item-has-children"><a href="service.html">Services</a>
+                  <ul>
+                    <li><a href="service.html">Services</a></li>
+                    <li><a href="service-details.html">Service Details</a></li>
+                  </ul>
+                </li>
+                <li class="menu-item-has-children"><a href="portfolio.html">Portfolio</a>
+                  <ul>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="portfolio-details.html">Portfolio Details</a></li>
+                  </ul>
+                </li>
+                <li class="menu-item-has-children"><a href="blog.html">Blog</a>
+                  <ul>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="blog-details.html">Blog Details</a></li>
+                  </ul>
+                </li>
+                <li class="menu-item-has-children">
+                  <a href="shop.html">Shop</a>
+                  <ul>
+                    <li>
+                      <a href="shop.html">Our Shop</a>
+                    </li>
+                    <li>
+                      <a href="shop-product-details.html">Shop Details</a>
+                    </li>
+                    <li>
+                      <a href="shop-cart.html">Cart</a>
+                    </li>
+                    <li>
+                      <a href="shop-checkout.html">Checkout</a>
+                    </li>
+                    <li>
+                      <a href="shop-order-recived.html">Success Order</a>
+                    </li>
+                    <li>
+                      <a href="shop-wishlist.html">Wishlist</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="menu-item-has-children"><a href="#">Pages</a>
+                  <ul>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="team.html">Team</a></li>
+                    <li><a href="team-details.html">Team Details</a></li>
+                    <li><a href="case-study.html">Case Study Details</a></li>
+                    <li><a href="faq.html">FAQ</a></li>
+                  </ul>
+                </li-->
+              </ul>
+            </div>
+          </div>
+          <div class="cs-main_header_right">
+            <div class="cs-toolbox">
+              <span class="cs-icon_btn">
+                <span class="cs-icon_btn_in">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
