d159d36af98cf83a67711b4f56b99267a6c9e4c6
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-11-16 00:47:30 -0300

    add link to contact us

--- front-page.php
+++ front-page.php
@@ -24,14 +24,14 @@
         <div class="cs-hero_info">
             <div>
               <a href="<?php echo home_url( '/contact/' ); ?>" class="cs-text_btn">
-                <span>Get a Quote</span>
+                <span>Request a Free Consultation</span>
                 <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor" />
                 </svg>
               </a>
             </div>
             <div>
-              <div class="cs-hero_subtitle">We deliver best problem solving solution for our client and provide finest finishing product in present and upcoming future.</div>
+              <div class="cs-hero_subtitle">We create high-performing websites and digital marketing strategies that boost your business</div>
             </div>
         </div>
       </div>
@@ -126,12 +126,12 @@
             <h3 class="cs-section_subtitle">Why Choose Us</h3>
             <h2 class="cs-section_title">We listen to what our clients need</h2>
             <div class="cs-height_45 cs-height_lg_20"></div>
-            <!--a href="about.html" class="cs-text_btn wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
+            <a href="about.html" class="cs-text_btn wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
               <span>Learn More</span>
               <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
               </svg>                
-            </a-->
+            </a>
           </div>
           <div class="cs-height_45 cs-height_lg_45"></div>
         </div>
