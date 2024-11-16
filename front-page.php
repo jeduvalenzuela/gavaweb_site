fbb2622c78246ecd83d0f3368631f8a828258a0b
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-11-16 01:02:10 -0300

    add link to contact us

--- front-page.php
+++ front-page.php
@@ -116,8 +116,8 @@
 
   <!-- Start Video Block Section -->
   <section class="cs-shape_wrap_4 cs-parallax">
-    <div class="cs-shape_4 cs-to_up"></div>
-    <div class="cs-shape_4 cs-to_right"></div>
+    <div class="cs-shape_4 cs-to_up" style="background:#ff0044"></div>
+    <div class="cs-shape_4 cs-to_right" style="background:#ff0044"></div>
     <div class="cs-height_150 cs-height_lg_80"></div>
     <div class="container">
       <div class="row">
@@ -128,12 +128,12 @@
 
             <div class="cs-hero_info">
               <div>
-                <div class="cs-hero_subtitle">Somos un equipo de expertos en desarrollo web con un enfoque especial en la creación de sitios personalizados y complejos en WordPress. Combinamos tecnología de vanguardia con estrategias de marketing digital para ofrecerte soluciones completas que garantizan resultados medibles.</div>
+                <div class="cs-hero_subtitle">We are a team of web development experts with a special focus on creating custom and complex WordPress sites. We combine cutting-edge technology with digital marketing strategies to offer you complete solutions that guarantee measurable results.</div>
               </div>
             </div>
 
             <div class="cs-height_45 cs-height_lg_20"></div>
-            <a href="about.html" class="cs-text_btn wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
+            <a href="<?php echo home_url('/about/'); ?>" class="cs-text_btn wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.2s">
               <span>Learn More</span>
               <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M25.5303 6.53033C25.8232 6.23744 25.8232 5.76256 25.5303 5.46967L20.7574 0.696699C20.4645 0.403806 19.9896 0.403806 19.6967 0.696699C19.4038 0.989593 19.4038 1.46447 19.6967 1.75736L23.9393 6L19.6967 10.2426C19.4038 10.5355 19.4038 11.0104 19.6967 11.3033C19.9896 11.5962 20.4645 11.5962 20.7574 11.3033L25.5303 6.53033ZM0 6.75H25V5.25H0V6.75Z" fill="currentColor"></path>
