30a078e7148bac62ab9f6c42026fa96b1d85dc85
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-11-20 10:41:53 -0300

    use loop to show related post

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function add_google_tag_to_head() {
+    ?>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4RB72P4MEQ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4RB72P4MEQ');
+    </script>
+    <?php
+}
+add_action('wp_head', 'add_google_tag_to_head');
