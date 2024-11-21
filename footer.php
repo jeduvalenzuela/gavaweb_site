e36f53a756f54b42c78b4d5c8605d1620356b900
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-11-21 10:19:47 -0300

    add link to contact us

--- footer.php
+++ footer.php
@@ -138,7 +138,7 @@
 </a>
 
   <!-- Script -->
-  <!--script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script-->
+  <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
   <script src="<?php echo get_bloginfo( 'template_directory' );?>/assets/js/plugins/jquery-3.6.0.min.js"></script>
   <script src="<?php echo get_bloginfo( 'template_directory' );?>/assets/js/plugins/isotope.pkg.min.js"></script>
   <script src="<?php echo get_bloginfo( 'template_directory' );?>/assets/js/plugins/jquery.slick.min.js"></script>
