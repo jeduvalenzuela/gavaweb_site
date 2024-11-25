f4165b67e3fbb2c5666d8231abdc1cd93c08b0dd
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-11-25 11:02:13 -0300

    add archive post

--- archive.php
+++ archive.php
@@ -20,28 +20,49 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8">
-            <div class="cs-post cs-style2">
-              <a href="blog-details.html" class="cs-post_thumb cs-radius_15">
-                <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/post_4.jpg" alt="Post" class="w-100 cs-radius_15">
-              </a>
-              <div class="cs-post_info">
-                <div class="cs-post_meta cs-style1 cs-ternary_color cs-semi_bold cs-primary_font">
-                  <span class="cs-posted_by">07 Mar 2022</span>
-                  <a href="#" class="cs-post_avatar">Tech</a>
-                </div>
-                <h2 class="cs-post_title">
-                  <a href="#">A.I will take all human job within next year</a>
-                </h2>
-                <div class="cs-post_sub_title">Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Tortor posuere ac ut consequat semper viverra nam libero justo. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Purus sit amet luctus venenatis lectus. Nunc aliquet bibendum enim facilisis. Pretium viverra suspendisse potenti nullam ac tortor vitae.</div>
-                <a href="blog-details.html" class="cs-text_btn">
-                  <span>See More</span>
-                  <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M25.5307 6.53033C25.8236 6.23744 25.8236 5.76256 25.5307 5.46967L20.7577 0.696699C20.4648 0.403806 19.99 0.403806 19.6971 0.696699C19.4042 0.989593 19.4042 1.46447 19.6971 1.75736L23.9397 6L19.6971 10.2426C19.4042 10.5355 19.4042 11.0104 19.6971 11.3033C19.99 11.5962 20.4648 11.5962 20.7577 11.3033L25.5307 6.53033ZM0.000366211 6.75H25.0004V5.25H0.000366211V6.75Z" fill="currentColor" />
-                  </svg>
+
+          <?php if (have_posts()) : 
+            while (have_posts()) : the_post(); ?>
+
+              <div class="cs-post cs-style2">
+                <a href="blog-details.html" class="cs-post_thumb cs-radius_15">
+                  <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/post_4.jpg" alt="Post" class="w-100 cs-radius_15">
                 </a>
+                <div class="cs-post_info">
+                  <div class="cs-post_meta cs-style1 cs-ternary_color cs-semi_bold cs-primary_font">
+                    <span class="cs-posted_by">07 Mar 2022</span>
+                    <a href="#" class="cs-post_avatar">Tech</a>
+                  </div>
+                  <h2 class="cs-post_title">
+                    <a href="#">A.I will take all human job within next year</a>
+                  </h2>
+                  <div class="cs-post_sub_title">Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Tortor posuere ac ut consequat semper viverra nam libero justo. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Purus sit amet luctus venenatis lectus. Nunc aliquet bibendum enim facilisis. Pretium viverra suspendisse potenti nullam ac tortor vitae.</div>
+                  <a href="blog-details.html" class="cs-text_btn">
+                    <span>See More</span>
+                    <svg width="26" height="12" viewBox="0 0 26 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M25.5307 6.53033C25.8236 6.23744 25.8236 5.76256 25.5307 5.46967L20.7577 0.696699C20.4648 0.403806 19.99 0.403806 19.6971 0.696699C19.4042 0.989593 19.4042 1.46447 19.6971 1.75736L23.9397 6L19.6971 10.2426C19.4042 10.5355 19.4042 11.0104 19.6971 11.3033C19.99 11.5962 20.4648 11.5962 20.7577 11.3033L25.5307 6.53033ZM0.000366211 6.75H25.0004V5.25H0.000366211V6.75Z" fill="currentColor" />
+                    </svg>
+                  </a>
+                </div>
               </div>
+              <div class="cs-height_95 cs-height_lg_60"></div>
+
+            <?php endwhile; ?>
+
+            <div class="pagination">
+                <?php
+                the_posts_pagination(array(
+                    'mid_size'  => 2,
+                    'prev_text' => __('&laquo; Previous', 'textdomain'),
+                    'next_text' => __('Next &raquo;', 'textdomain'),
+                ));
+                ?>
             </div>
-            <div class="cs-height_95 cs-height_lg_60"></div>
+
+          <?php else : ?>
+            <p>No posts found.</p>
+          <?php endif; ?>
+
             <div class="cs-post cs-style2">
               <a href="blog-details.html" class="cs-post_thumb cs-radius_15">
                 <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/post_5.jpg" alt="Post" class="w-100 cs-radius_15">
@@ -64,6 +85,7 @@
               </div>
             </div>
             <div class="cs-height_95 cs-height_lg_60"></div>
+
             <div class="cs-post cs-style2">
               <a href="blog-details.html" class="cs-post_thumb cs-radius_15">
                 <img src="<?php echo get_bloginfo( 'template_directory' );?>/assets/img/post_6.jpg" alt="Post" class="w-100 cs-radius_15">
@@ -86,6 +108,7 @@
               </div>
             </div>
             <div class="cs-height_60 cs-height_lg_40"></div>
+
             <ul class="cs-pagination_box cs-center cs-white_color cs-mp0 cs-semi_bold">
               <li>
                 <a class="cs-pagination_item cs-center active" href="blog.html">1</a>
@@ -107,6 +130,7 @@
                 </a>
               </li>
             </ul>
+
           </div>
           <div class="col-xl-3 col-lg-4 offset-xl-1">
             <div class="cs-height_0 cs-height_lg_80"></div>
